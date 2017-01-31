e4946f771010fc808d4e696ceabd0dd284930b2e
RaulCM <raulcanomontero@hotmail.com>
2017-01-31 12:51:58 +0100

    Update FicherosYListas.py

--- FicherosYListas.py
+++ FicherosYListas.py
@@ -5,20 +5,20 @@ fd.close()
 # lineas[0] # 'pepepepepepepepe' String
 
 # Forma 1
-for linea in lineas:
-	pos_fin = linea.find(":")
-	login = linea[:pos_fin]
-	pos_com = linea.rfind(":") + 1
-	shell = linea[pos_com:-1]
-	print(login, shell)
+# for linea in lineas:
+#	pos_fin = linea.find(":")
+#	login = linea[:pos_fin]
+#	pos_com = linea.rfind(":") + 1
+#	shell = linea[pos_com:-1]
+#	print(login, shell)
 
 # Forma 2
-for linea in lineas:
-	elementos = linea.split(":")
-	login = elementos[0]
-	shell = elementos[-1]
-	shell_bueno = shell[:-1]
-	print(login, shell_bueno)
+# for linea in lineas:
+#	elementos = linea.split(":")
+#	login = elementos[0]
+#	shell = elementos[-1]
+#	shell_bueno = shell[:-1]
+#	print(login, shell_bueno)
 
 # Forma 3
 for linea in lineas:
